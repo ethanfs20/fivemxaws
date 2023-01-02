7f6b320aec07ee3069f0448e5bd09f397b04fbe7
Ethan Shearer <ethan@theshearerfamily.com>
2023-01-02 20:19:15 +0000

    s

--- setup.sh
+++ setup.sh
@@ -20,13 +20,8 @@
 #     \/ \__,_|_|  |_|\__,_|_.__/|_|\___||___/ #
 # -------------------------------------------- #                                                                                                              
 
-<<<<<<< HEAD
 Access_Key="REPLACE ME!!!" # Replace with your own Access Key
 Secret_Access_Key="REPLACE ME!!!" # Replace with your own Secret Access Key
-=======
-Access_Key="REPLACEME" # Replace with your own Access Key
-Secret_Access_Key="REPLACEME" # Replace with your own Secret Access Key
->>>>>>> fd2420b2fbd6ab1053d23b6fe7cf9bd95d340842
 Default_Region_Name="us-east-1" # Region name for your AWS account (e.g. us-east-1). https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html#concepts-available-regions
 Default_Output_Format="json" # Output format for AWS CLI (e.g. json, text, table). https://docs.aws.amazon.com/cli/latest/userguide/cli-usage-output-format.html
 AMI_ID="ami-08e637cea2f053dfa" # AMI ID for the Amazon Linux 2 AMI. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/AMIs.html
